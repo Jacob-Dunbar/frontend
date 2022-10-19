878877c4fdc5e63bb7b57a12e1aba28f79dc86eb
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-19 23:07:42 +0100

    style: update header styling

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
 import styles from "../styles/header.module.css";
+import Image from "next/image";
+import myFace from "../public/myface.jpg";
 
 function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.imageWrapper}>
+        <Image src={myFace} />
+      </div>{" "}
+      <h1>Jacob Dunbar</h1>
+      <h2>Web Developer</h2>
+    </header>
+  );
 }
 
 export default Header;
