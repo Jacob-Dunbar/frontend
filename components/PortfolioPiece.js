b22d8644015859ab16c2422ed9db4ebeb6021f52
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-19 18:09:25 +0100

    style: add prelim styles to portfolioCards

--- components/PortfolioPiece.js
+++ components/PortfolioPiece.js
@@ -1,4 +1,4 @@
-import React from "react";
+import styles from "../styles/portfoliopiece.module.css";
 
 function PortfolioPiece(props) {
   const imageUrl = props.image.data[0].attributes.url;
@@ -6,14 +6,16 @@ function PortfolioPiece(props) {
 
   console.log(techStack);
   return (
-    <div className="portfolioCard">
-      {/* <img src={imageUrl} alt="thumbnail" /> */}
+    <div className={styles.portfolioCard}>
+      <img src={imageUrl} alt="thumbnail" />
       <div className="body">
         <h1>{props.title}</h1>
         <p>{props.description}</p>
-        {techStack.map((item) => (
-          <h2>helo</h2>
-        ))}
+        <div className={styles.stack}>
+          {techStack.map((item) => (
+            <h2>helo</h2>
+          ))}
+        </div>
       </div>
     </div>
   );
