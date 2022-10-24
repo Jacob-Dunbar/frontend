b272ced31197dbd27cba2ffab47c7c1c077a84b3
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-24 18:51:27 +0100

    feat: set polar and azimuthal angle to state.mouse

--- components/ThreeHeader.js
+++ components/ThreeHeader.js
@@ -1,29 +1,26 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+
 import { Suspense, useRef, useState } from "react";
-import { OrbitControls, useGLFT } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Face from "./Face";
 import css from "../styles/three.module.css";
 import Skull4 from "../components/Skull4";
 import { useSpring, animated } from "@react-spring/three";
 
-export default function R3FDemo() {
-  const ref = useRef();
-
+export default function ThreeHeader() {
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
-  const props = useSpring({
-    intensity: active ? 1 : 0.6,
-  });
 
-  console.log("hello");
   return (
     <div className={css.canvas}>
-      <Canvas>
+      {/* light mode face vvv */}
+
+      {/* <Canvas>
         <Suspense fallback={null}>
           <ambientLight intensity={0.4} />
-          <OrbitControls />
+
           <animated.spotLight
-            intensity={props.intensity}
+            intensity={0.6}
             angle={0.9}
             penumbra={1}
             position={[10, 50, 160]}
@@ -35,7 +32,26 @@ export default function R3FDemo() {
             onPointerOver={() => setHovered(true)}
             onPointerOut={() => setHovered(false)}
             onClick={() => setActive(!active)}
-            scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+            // scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+            hovered={hovered}
+            active={active}
+          />
+        </Suspense>
+      </Canvas> */}
+
+      {/* dark mode skull vvv */}
+
+      <Canvas>
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.4} />
+
+          <animated.spotLight
+            intensity={0.6}
+            angle={0.9}
+            penumbra={1}
+            position={[10, 50, 160]}
+            castShadow
+            color={"#FFFFFF"}
           />
         </Suspense>
       </Canvas>
