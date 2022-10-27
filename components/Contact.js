556d491ae1b65dd561abe6c6a3b29a12d0c336bb
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-27 16:56:04 +0100

    feat: add form validation to contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,27 +1,93 @@
-import React from "react";
+import { useState } from "react";
 import css from "../styles/contact.module.scss";
 
 function Contact(props) {
-  async function handleSubmit(e) {
+  //form data object
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  //field validation states
+  const [nameVal, setNameVal] = useState(true);
+  const [messageVal, setMessageVal] = useState(true);
+  const [emailVal, setEmailVal] = useState(true);
+
+  // handle submit - check fields are not empty and that corresponding state is true
+  // if any empty fields set corresponding state to false
+  // if all good post to mail api
+  function handleSubmit(e) {
     e.preventDefault();
-    const formData = {};
-    const elementArray = Array.from(e.target.elements);
-    elementArray.forEach((field) => {
-      if (!field.name) {
-        return;
-      } else {
-        formData[field.name] = field.value;
-      }
-    });
 
-    fetch("/api/mail", {
-      method: "post",
-      body: JSON.stringify(formData),
-    });
-
-    console.log(formData);
+    if (!formData.name || !nameVal) {
+      setNameVal(false);
+    } else if (!formData.email || !emailVal) {
+      setEmailVal(false);
+    } else if (!formData.message || !messageVal) {
+      setMessageVal(false);
+    } else if (
+      formData.name &&
+      formData.email &&
+      formData.message &&
+      nameVal &&
+      emailVal &&
+      messageVal
+    ) {
+      fetch("/api/mail", {
+        method: "post",
+        body: JSON.stringify(formData),
+      });
+      console.log("sent");
+    }
   }
 
+  //handle change
+  //spread prev formData and over write current target data
+  //use [] to allow dynamic string to be used as key
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+    // set validation state to true if value truthy.
+    if (event.target.name === "name" && event.target.value) {
+      setNameVal(true);
+    } else if (event.target.name === "message" && event.target.value) {
+      setMessageVal(true);
+    } else if (event.target.name == "email" && event.target.value) {
+      setEmailVal(true);
+    }
+  }
+
+  //check field functions - all fields for contents and check email for @
+  function checkNameField(event) {
+    if (!event.target.value) {
+      setNameVal(false);
+    } else {
+      setNameVal(true);
+    }
+  }
+
+  function checkMessageField(event) {
+    if (!event.target.value) {
+      setMessageVal(false);
+    } else {
+      setMessageVal(true);
+    }
+  }
+
+  function checkEmail(event) {
+    if (!event.target.value.includes("@")) {
+      setEmailVal(false);
+    } else {
+      setEmailVal(true);
+    }
+  }
+
+  //remember to control inputs so formData state drives the input state and they stay insync. value={formData.input}
   return (
     <div className={css.form_container}>
       <h1 className={props.darkMode ? css.heading_dark : css.heading_light}>
@@ -34,16 +100,41 @@ function Contact(props) {
       >
         <p>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+          <input
+            className={!nameVal ? css.field_err : undefined}
+            onChange={handleChange}
+            type="text"
+            name="name"
+            value={formData.name}
+            onBlur={checkNameField}
+          />
         </p>
+        <p className={css.err_text}>{!nameVal && "Introduce yourself"}</p>
         <p>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input
+            className={!emailVal ? css.field_err : undefined}
+            onChange={handleChange}
+            type="email"
+            name="email"
+            value={formData.email}
+            onBlur={checkEmail}
+          />
+        </p>
+        <p className={css.err_text}>
+          {!emailVal && "Thats not a valid email address!"}
         </p>
         <p>
           <label htmlFor="message">Message</label>
-          <textarea name="message" />
+          <textarea
+            className={!messageVal ? css.field_err : undefined}
+            onChange={handleChange}
+            name="message"
+            value={formData.message}
+            onBlur={checkMessageField}
+          />
         </p>
+        <p className={css.err_text}>{!messageVal && "Don't be shy!"}</p>
         <p className={css.button_container}>
           <button>Send Message</button>
         </p>
